b1619bcc8e988a680a8838708b94a3410f5c242d
Jongjate Choomjairug <jongjate.ch@gmail.com>
2022-07-02 09:17:43 +0700

    Done

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,28 +1,48 @@
 import './Calculator.css'
+import {useState} from "react"
+
 const Calculator =()=>{
+    const [inputValue,setInputValue] = useState("0")
+
+    const display =(value)=>{
+        if(inputValue.toString().charAt(0) === "0") {
+            setInputValue(value)
+        } else {
+            setInputValue(inputValue+value)
+        }
+    }
+
+    const reset =()=>{
+        setInputValue("0")
+    }
+
+    const calculate =()=>{    
+        setInputValue(eval(inputValue))
+    }
+
     return(
         <div className="calculator">
             <div className="calculator-display">
-                <h1>0</h1>
+                <h1>{inputValue}</h1>
             </div>
             <div className="calculator-button">
-                <button className="operator">+</button>
-                <button className="operator">-</button>
-                <button className="operator">x</button>
-                <button className="operator">/</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>.</button>
-                <button>0</button>
-                <button className="clear-btn">C</button>
-                <button className="equal operator">=</button>
+                <button onClick={()=>display("+")} className="operator">+</button>
+                <button onClick={()=>display("-")} className="operator">-</button>
+                <button onClick={()=>display("*")} className="operator">x</button>
+                <button onClick={()=>display("/")} className="operator">÷</button>
+                <button onClick={()=>display("7")}>7</button>
+                <button onClick={()=>display("8")}>8</button>
+                <button onClick={()=>display("9")}>9</button>
+                <button onClick={()=>display("4")}>4</button>
+                <button onClick={()=>display("5")}>5</button>
+                <button onClick={()=>display("6")}>6</button>
+                <button onClick={()=>display("1")}>1</button>
+                <button onClick={()=>display("2")}>2</button>
+                <button onClick={()=>display("3")}>3</button>
+                <button onClick={()=>display(".")}>.</button>
+                <button onClick={()=>display("0")}>0</button>
+                <button onClick={()=>reset()}className="clear-btn">C</button>
+                <button onClick={()=>calculate()} className="equal operator">=</button>
             </div>
         </div>
     )
